fcce0b8f54c274821538c443bb0cc606cdc1acd1
Cristina Nazário <dematosnazariocristina@gmail.com>
2025-02-28 15:13:24 -0300

    Ajuste no nome do teste

--- test/exercicio-3.test.js
+++ test/exercicio-3.test.js
@@ -2,7 +2,7 @@ const somarTodosMultiplos = require('../exercicio-3')
 
 describe('Exercício 3 - Soma de múltiplos', () => {
     
-    it('Deve retornar 156361 para multiplos de 5 e 7', () => {
+    it('Deve retornar 156361 para multiplos de 5 e 7 somados', () => {
         expect(somarTodosMultiplos(1000, 5, 7)).toBe(156361)
     });
 
